5a021fe5698f5b62c2df6c7c537fcc9c5293b61a
ErickSkrauch <erickskrauch@yandex.ru>
2017-05-21 01:01:59 +0300

    Добавлены новые языки: el, fil, id, pl, ro, sl, vi. Пул существующих переводов.

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,30 @@ import { PropTypes } from 'react';
 import { IntlProvider, addLocaleData } from 'react-intl';
 
 import beLocaleData from 'react-intl/locale-data/be';
+import elLocaleData from 'react-intl/locale-data/el';
 import enLocaleData from 'react-intl/locale-data/en';
+import filLocaleData from 'react-intl/locale-data/fil';
+import idLocaleData from 'react-intl/locale-data/id';
+import plLocaleData from 'react-intl/locale-data/pl';
+import roLocaleData from 'react-intl/locale-data/ro';
 import ruLocaleData from 'react-intl/locale-data/ru';
+import slLocaleData from 'react-intl/locale-data/sl';
 import ptLocaleData from 'react-intl/locale-data/pt';
 import ukLocaleData from 'react-intl/locale-data/uk';
+import viLocaleData from 'react-intl/locale-data/vi';
 
 addLocaleData(beLocaleData);
+addLocaleData(elLocaleData);
 addLocaleData(enLocaleData);
+addLocaleData(filLocaleData);
+addLocaleData(idLocaleData);
+addLocaleData(plLocaleData);
+addLocaleData(roLocaleData);
 addLocaleData(ruLocaleData);
+addLocaleData(slLocaleData);
 addLocaleData(ptLocaleData);
 addLocaleData(ukLocaleData);
+addLocaleData(viLocaleData);
 
 import { SUPPORTED_LANGUAGES, DEFAULT_LANGUAGE } from './constants';
 
