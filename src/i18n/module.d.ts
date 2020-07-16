5dc8aae90c08cbcf211a91d13b60b49c816e3762
ErickSkrauch <erickskrauch@yandex.ru>
2020-07-16 15:53:35 +0300

    Pull locales

--- src/i18n/module.d.ts
+++ src/i18n/module.d.ts
@@ -1,12 +1,10 @@
 declare module 'i18n/index.json' {
-    const content: {
-        [key: string]: {
-            code: string;
-            name: string;
-            englishName: string;
-            progress: number;
-            isReleased: boolean;
-        };
-    };
+    const content: Record<string, {
+        code: string;
+        name: string;
+        englishName: string;
+        progress: number;
+        isReleased: boolean;
+    }>;
     export default content;
 }
